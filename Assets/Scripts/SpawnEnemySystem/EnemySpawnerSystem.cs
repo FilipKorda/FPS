43cba6a4cdafa3d158f21be18f4ce63df20419ac
FilipKorda <filipkorda00@gmail.com>
2024-08-18 21:38:16 +0200

    Update

--- Assets/Scripts/SpawnEnemySystem/EnemySpawnerSystem.cs
+++ Assets/Scripts/SpawnEnemySystem/EnemySpawnerSystem.cs
@@ -1,3 +1,4 @@
+using FPS.Guns.Demo;
 using System.Collections;
 using UnityEngine;
 
@@ -7,6 +8,10 @@ public class EnemySpawnerSystem : MonoBehaviour
     [SerializeField] private Transform[] spawnPoints;
     [SerializeField] private float firstSpawnDelay = 5f;
     [SerializeField] private ParticleSystem spawnParticles;
+
+    //Camera shake to alarm player
+    [SerializeField] private float cameraShakeDuration = 0.75f;
+    [SerializeField] private float cameraShakeMagnitude = 0.1f;
 
     private void Start()
     {
@@ -21,12 +26,38 @@ public class EnemySpawnerSystem : MonoBehaviour
     private IEnumerator SpawnEnemies()
     {
         yield return new WaitForSeconds(firstSpawnDelay);
-
+        AlarmPlayer();
         foreach (var spawnPoint in spawnPoints)
         {
             ParticleSystem particles = Instantiate(spawnParticles, spawnPoint.position, spawnPoint.rotation);
             particles.Play();
             Instantiate(enemy.enemyPrefab, spawnPoint.position, spawnPoint.rotation);
         }
+    }
+
+    private void AlarmPlayer()
+    {
+        StartCoroutine(Shake(cameraShakeDuration, cameraShakeMagnitude));
+    }
+
+    public IEnumerator Shake(float duration, float magnitude)
+    {
+        Vector3 originalPos = PlayerGunSelector.Instance.Camera.transform.localPosition;
+
+        float elapsed = 0.0f;
+
+        while (elapsed < duration)
+        {
+            float x = Random.Range(-1f, 1f) * magnitude;
+            float y = Random.Range(-1f, 1f) * magnitude;
+
+            PlayerGunSelector.Instance.Camera.transform.localPosition = new Vector3(x, y, originalPos.z);
+
+            elapsed += Time.deltaTime;
+
+            yield return null;
+        }
+
+        transform.localPosition = originalPos;
     }
 }
